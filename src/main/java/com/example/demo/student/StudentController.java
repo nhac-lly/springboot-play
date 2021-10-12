27b72085c40df8da2328eebda230c62603011bd2
Nyak <nhac.ly93@gmail.com>
2021-10-12 16:01:44 +0700

    update

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,2 +1,23 @@
-package com.example.demo.student;public class StudentController {
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudent() {
+        return studentService.getStudent();
+    }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,24 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudent() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Nhac",
+                        "nhac.ly@hotmail.com",
+                        LocalDate.of(1993, Month.SEPTEMBER, 6),
+                        28
+                )
+        );
+    }
 }
